9d18303906c8539dda88e580886f23b0ffa355a9
maisotro <maisotro@gmail.com>
2021-09-23 10:22:07 -0300

    Add files via upload

---
+++ heitorrss.py
@@ -0,0 +1,68 @@
+import feedparser
+import fire
+import twint
+import pyperclip
+import os
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+def twitch():
+    d = feedparser.parse("https://twitchrss.appspot.com/vod/heitorloureiro")
+    for entry in d.entries:
+        soup = BeautifulSoup(entry.summary, 'lxml')
+        a = soup.find('img')
+        url = a['src']
+        r = urllib.request.urlopen(url)
+        with open("heitor.png", "wb") as f:
+            f.write(r.read())
+        a = d.entries[0].title + "\n" + d.entries[0].link
+        print(str(a))
+        os.system(
+            "xclip -sel clip -t image/png /home/pedro/Documentos/Python/Heitor/heitor.png")
+        break
+
+
+def blog():
+    d = feedparser.parse("https://medium.com/feed/@heitorloureiro")
+    print(d.entries[0].title + "\n" + d.entries[0].link)
+    return d.entries[0].title + "\n" + d.entries[0].link
+
+
+def twitter():
+    c = twint.Config()
+    c.Username = "doutorheitor"
+    c.Limit = 1
+    return twint.run.Search(c)
+
+
+def podcast():
+    d1 = feedparser.parse("https://ondem.libsyn.com/rss")
+    for entry in d1.entries:
+        if "ONDE Política" in str(entry.title):
+            return entry.title + "\n" + entry.link
+            break
+
+
+def youtube():
+    d2 = feedparser.parse(
+        "https://www.youtube.com/feeds/videos.xml?channel_id=UCagwwqpZie4J4cXerPfO1bw")
+    for entry in d2.entries:
+        return entry.title + "\n" + entry.link
+
+
+def bot():
+    # medium
+    a = blog()
+    b = twitter()
+    c = podcast()
+    d = youtube()
+
+    a = '*#######--Heitor Stalker 1.0--#######*\n\n\n *Ultimo post do Blog* \n' + \
+        str(a)+'\n*Posts do twitter*\n'+str(b) +\
+        '\n*Ultimo episodio do podcast* \n' + \
+        str(c)+'\n*Ultimo video no youtube* '+str(d)
+    pyperclip.copy(a)
+
+
+fire.Fire()
---
+++ heitorstalker01.py
@@ -0,0 +1,70 @@
+import feedparser
+from tkinter import Tk, ttk, Text
+import pyperclip
+from ttkthemes import ThemedTk
+import sys
+
+
+def Stalker(evento=None):
+    conteudo.configure(state='normal')
+    conteudo.delete('1.0', 'end')
+
+    if combo.get() == 'Blog':
+        d = feedparser.parse("https://medium.com/feed/@heitorloureiro")
+        texto = d.entries[0].title + "\n" + d.entries[0].link
+        conteudo.insert('1.0', texto)
+
+    if combo.get() == 'Podcast':
+        d1 = feedparser.parse("https://ondem.libsyn.com/rss")
+        for entry in d1.entries:
+            if "ONDE Política" in str(entry.title):
+                texto = entry.title + "\n" + entry.link
+                conteudo.insert('1.0', texto)
+                break
+
+    if combo.get() == 'Youtube':
+        d2 = feedparser.parse(
+            "https://www.youtube.com/feeds/videos.xml?channel_id=UCagwwqpZie4J4cXerPfO1bw")
+        for entry in d2.entries:
+            texto = entry.title + "\n" + entry.link
+            conteudo.insert('1.0', texto)
+    conteudo.configure(state='disabled')
+
+
+def sair():
+    sys.exit(0)
+
+
+def copia():
+    texto = conteudo.get(1.0, 'end')
+    pyperclip.copy(texto)
+
+
+# janela = Tk()
+janela = ThemedTk(theme='arc')
+janela.title('Heitor Stalker 0.1')
+janela.geometry('870x470')
+
+frame = ttk.Frame()
+conteudo = Text(frame)
+
+
+values = ['Blog', 'Podcast', 'Youtube']
+
+combo = ttk.Combobox(frame, values=values)
+combo['state'] = 'readonly'
+combo.set('Blog')
+
+botao = ttk.Button(frame, text="Stalqueie", command=Stalker)
+butao = ttk.Button(frame, text="Sair", command=sair)
+butao_copia = ttk.Button(frame, text="Copiar", command=copia)
+
+botao.grid(row=0, column=1, padx=4)
+butao.grid(row=1, column=1)
+butao_copia.grid(row=1, column=3)
+combo.grid(row=0, column=0, pady=4)
+conteudo.grid(row=1, column=0, padx=4)
+frame.pack()
+
+janela.bind('<Return>', Stalker)
+janela.mainloop()
