a2c5a5593488348ec997f0a9c9f7a49661d08a43
Pedro <maisotro@gmail.com>
2022-01-08 12:08:26 -0300

    Arquivo de teste do bot

---
+++ testes.py
@@ -0,0 +1,18 @@
+import feedparser
+import re
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+
+d1 = feedparser.parse("https://ondem.libsyn.com/rss")
+for entry in d1.entries:
+    if "ONDE Política" in str(entry.title):
+        #texto = entry.title + "\n" + entry.link + '\n' + entry.subtitle + '\n' + entry.id
+        texto = entry.content
+        print(remove_tags(str(texto)))
+        break
+        
+
+
