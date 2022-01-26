f1433b34d36472aa289063630ec5c6c1924ff4f9
Pedro <maisotro@gmail.com>
2022-01-26 11:26:44 -0300

    retirando a  biblioteca twint

--- heitorrss.py
+++ heitorrss.py
@@ -10,14 +10,6 @@ def blog():
     d = feedparser.parse("https://medium.com/feed/@heitorloureiro")
     return d.entries[0].title + "\n" + d.entries[0].link
 
-
-def twitter():
-    c = twint.Config()
-    c.Username = "doutorheitor"
-    c.Limit = 1
-    return twint.run.Search(c)
-
-
 def podcast():
     d1 = feedparser.parse("https://ondem.libsyn.com/rss")
     for entry in d1.entries:
@@ -25,23 +17,20 @@ def podcast():
             return entry.title + "\n" + entry.link
             break
 
-
 def youtube():
     d2 = feedparser.parse(
         "https://www.youtube.com/feeds/videos.xml?channel_id=UCagwwqpZie4J4cXerPfO1bw")
     for entry in d2.entries:
         return entry.title + "\n" + entry.link
 
-
 def bot():
     # medium
     a = blog()
-    b = twitter()
     c = podcast()
     d = youtube()
 
     a = '*#######--Heitor Stalker 1.0--#######*\n\n\n *Ultimo post do Blog* \n' + \
-        str(a)+'\n*Posts do twitter*\n'+str(b) +\
+        str(a) +\
         '\n*Ultimo episodio do podcast* \n' + \
         str(c)+'\n*Ultimo video no youtube* '+str(d)
     pyperclip.copy(a)
